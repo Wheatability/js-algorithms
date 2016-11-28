87f5df646ccc8cfd9d701b52b081abf67188de6b
Wheatability <kileki@gmail.com>
2016-11-28 18:51:45 -0500

    beautify file

--- algorithms/titleCase.js
+++ algorithms/titleCase.js
@@ -5,10 +5,10 @@ function titleCase(str) {
   // Convert old string to lower case and split into array of words
   var wordArr = str.toLowerCase().split(' ');
   // Build our new string one word at a time making the first letter capitalized.
-  for(var i = 0; i < wordArr.length; i++) {
+  for (var i = 0; i < wordArr.length; i++) {
     newStr += wordArr[i][0].toUpperCase() + wordArr[i].slice(1);
     // Add a space after each word unless it is the last word in the string.
-    if(i < wordArr.length - 1) {
+    if (i < wordArr.length - 1) {
       newStr += ' ';
     }
   }
@@ -18,7 +18,7 @@ function titleCase(str) {
 
 // Using built-in functions (more elegant)
 function titleCase(str) {
-  return str.toLowerCase().split(' ').map(function(word){
+  return str.toLowerCase().split(' ').map(function (word) {
     return word.replace(word[0], word[0].toUpperCase());
   }).join(' ');
 }
