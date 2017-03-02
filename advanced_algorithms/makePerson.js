4ca7066a6c6b787ef9f66c215ded9143743418e1
Wheatability <kileki@gmail.com>
2017-03-02 14:41:03 -0500

    add semicolons and fix typo

--- advanced_algorithms/makePerson.js
+++ advanced_algorithms/makePerson.js
@@ -3,29 +3,29 @@ var Person = function(firstAndLast) {
   
   this.getFirstName = () => {
     return nameArr[0];
-  }
+  };
 
   this.getLastName = () => {
     return nameArr[1];
-  }
+  };
 
   this.getFullName = () => {
     return nameArr.join(' ');
-  }
+  };
 
-  this.setFirstName = (firs) => {
+  this.setFirstName = (first) => {
     nameArr[0] = first;
     return nameArr[0];
-  }
+  };
 
   this.setLastName = (last) => {
     nameArr[1] = last;
     return nameArr[1];
-  }
+  };
 
   this.setFullName = (firstAndLast) => {
     nameArr = firstAndLast.split(' ');
-  }
+  };
 };
 
 var bob = new Person('Bob Ross');
